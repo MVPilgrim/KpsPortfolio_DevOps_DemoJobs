577d0c94fd43158e5885b61cb2c9acaeec50fe1b
MVPilgrim <mv.pilgrim.aws@gmail.com>
2020-10-19 22:27:00 -0500

    Debug.

---
+++ DemoIoTButton.groovy
@@ -0,0 +1,55 @@
+#! /usr/bin/groovy
+
+/************************************************************
+/* Module: DemoIoTButton
+/*
+/* Description: Runs a Jenkins Job triggered by Amazon IoT 
+/*
+/* Change Log:
+/*   * KPS 10-17-2020: Initial version.
+/**********************************************************/
+
+
+import groovy.json.JsonSlurper
+
+//@Library('SharedLibrary') _
+@Library('KpsPortfolioSharedLibrary') _kpspflib
+
+
+node('Generic') {
+  try {
+    jobName = JOB_BASE_NAME;
+    initStage();
+	sendEmailStage();
+  } catch (Exception e) {
+    echo "Exception caught: " + e.getMessage();
+  }
+}
+
+
+/*
+ * Intialize stage.
+*/
+def initStage() {
+  stageName = "Init $jobName";
+  stage("$stageName") {
+    DisplayStageBanner(stageName);
+    sh "whoami";
+    env.PATH="$env.PATH:/opt/node-v12.19.0-linux-x64/bin";
+	ngnxDir="/usr/share/nginx";
+  }
+}
+
+
+/*
+ * Send E-mail back to initiator.
+*/
+def sendEmailStage() {
+  stageName = "Send Email"
+  stage("$stageName") {
+    DisplayStageBanner("$stageName");
+	sh """
+	  echo '###### Remember to add Email notification.'
+	"""
+  }
+}
